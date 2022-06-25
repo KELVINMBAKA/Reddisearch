e06ad0bb94ebd1f11dbeeff565d86adccfd89de5
KELVINMBAKA <loverc832@gmail.com>
2022-06-25 20:06:11 +0300

    Created an Event listerner for submit

---
+++ app.js
@@ -0,0 +1,9 @@
+//  Grab search form 
+const searchForm = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
+
+//Event listener for submit
+searchForm.addEventListener('submit', e => {
+
+        e.preventDefault();
+      });
