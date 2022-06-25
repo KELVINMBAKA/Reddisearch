baed7d53845330f09d74e06b58bbeff59e05bab7
KELVINMBAKA <loverc832@gmail.com>
2022-06-25 20:20:04 +0300

    Added err message if search input is empty

--- app.js
+++ app.js
@@ -20,5 +20,21 @@ const searchLimit = document.getElementById('limit').value;
       });
 
       function showMessage(message, className) {
-        
+          //Create div
+  const div = document.createElement('div');
+   //Add class to div
+   div.className = `alert ${className}`;
+   //Add text
+   div.appendChild(document.createTextNode(message));
+   //Get Parent of error message
+   const searchContainer = document.getElementById('search-container');
+   //Get sibling of error message
+   const search = document.getElementById('search');
+ 
+   //Insert message up there
+   searchContainer.insertBefore(div, search);
+ 
+   //error message Timeout
+   setTimeout(() => div.remove(), 3000);
+
       }
