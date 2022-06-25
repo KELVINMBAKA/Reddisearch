27740cb6a2fc79753df72b89404616f69aef7713
KELVINMBAKA <loverc832@gmail.com>
2022-06-25 20:14:53 +0300

    Message when search input is Empty

--- app.js
+++ app.js
@@ -4,6 +4,21 @@ const searchInput = document.getElementById('search-input');
 
 //Event listener for submit
 searchForm.addEventListener('submit', e => {
+      //Grab searched term
+  const searchTerm = searchInput.value;
+//Grab sortBy
+const sortBy = document.querySelector('input[name="sortby"]:checked').value;
+const searchLimit = document.getElementById('limit').value;
+
+  //Mkae sure no empty submisions
+  if(searchTerm === '') {
+    showMessage('Please add a search term', 'alert-danger');
+  }
+
 
         e.preventDefault();
       });
+
+      function showMessage(message, className) {
+        
+      }
